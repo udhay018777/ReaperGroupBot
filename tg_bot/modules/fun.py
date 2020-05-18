ed99aaafdaf06cf02c885b1b4af4add768a172b7
udhay018777 <63841073+udhay018777@users.noreply.github.com>
2020-05-19 00:34:49 +0530

    Add files via upload

---
+++ tg_bot/modules/fun.py
@@ -0,0 +1,118 @@
+import html
+import random
+import time
+from typing import List
+
+from telegram import Bot, Update, ParseMode
+from telegram.ext import run_async
+
+import tg_bot.modules.fun_strings as fun_strings
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.chat_status import is_user_admin
+from tg_bot.modules.helper_funcs.extraction import extract_user
+
+@run_async
+def ben(bot: Bot, update: Update):
+    update.message.reply_text(random.choice(fun_strings.BEN))
+
+@run_async
+def roll(bot: Bot, update: Update):
+    update.message.reply_text(random.choice(range(1, 7)))
+
+
+@run_async
+def toss(bot: Bot, update: Update):
+    update.message.reply_text(random.choice(fun_strings.TOSS))
+
+@run_async
+def rape(bot: Bot, update: Update):
+    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text(random.choice(fun_strings.RAPE))
+    
+@run_async
+def abuse(bot: Bot, update: Update):
+    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text(random.choice(fun_strings.ABUSE_STRINGS))
+    
+
+@run_async
+def shrug(bot: Bot, update: Update):
+    msg = update.effective_message
+    reply_text = msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
+    reply_text(r"¯\_(ツ)_/¯")
+
+
+@run_async
+def bluetext(bot: Bot, update: Update):
+    msg = update.effective_message
+    reply_text = msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
+    reply_text("/BLUE /TEXT\n/MUST /CLICK\n/I /AM /A /STUPID /ANIMAL /THAT /IS /ATTRACTED /TO /COLORS")
+
+
+@run_async
+def rlg(bot: Bot, update: Update):
+    eyes = random.choice(fun_strings.EYES)
+    mouth = random.choice(fun_strings.MOUTHS)
+    ears = random.choice(fun_strings.EARS)
+
+    if len(eyes) == 2:
+        repl = ears[0] + eyes[0] + mouth[0] + eyes[1] + ears[1]
+    else:
+        repl = ears[0] + eyes[0] + mouth[0] + eyes[0] + ears[1]
+    update.message.reply_text(repl)
+
+
+@run_async
+def decide(bot: Bot, update: Update):
+    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text(random.choice(fun_strings.DECIDE))
+
+@run_async
+def table(bot: Bot, update: Update):
+    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text(random.choice(fun_strings.TABLE))
+    
+
+__help__ = """
+ - /runs: reply a random string from an array of replies.
+ - /slap: slap a user, or get slapped if not a reply.
+ - /shrug : get shrug XD.
+ - /table : get flip/unflip :v.
+ - /decide : Randomly answers yes/no/maybe
+ - /toss : Tosses A coin
+ - /abuse : Abuses the cunt
+ - /bluetext : check urself :V
+ - /roll : Roll a dice.
+ - /rlg : Join ears,nose,mouth and create an emo ;-;
+ - /rape Rape anybody
+ - /ben Try it
+"""
+
+ROLL_HANDLER = DisableAbleCommandHandler("roll", roll)
+TOSS_HANDLER = DisableAbleCommandHandler("toss", toss)
+SHRUG_HANDLER = DisableAbleCommandHandler("shrug", shrug)
+BLUETEXT_HANDLER = DisableAbleCommandHandler("bluetext", bluetext)
+RLG_HANDLER = DisableAbleCommandHandler("rlg", rlg)
+DECIDE_HANDLER = DisableAbleCommandHandler("decide", decide)
+TABLE_HANDLER = DisableAbleCommandHandler("table", table)
+RAPE_HANDLER = DisableAbleCommandHandler("rape", rape)
+ABUSE_HANDLER = DisableAbleCommandHandler("abuse", abuse)
+BEN_HANDLER = DisableAbleCommandHandler("ben", ben)
+
+
+dispatcher.add_handler(ROLL_HANDLER)
+dispatcher.add_handler(TOSS_HANDLER)
+dispatcher.add_handler(SHRUG_HANDLER)
+dispatcher.add_handler(BLUETEXT_HANDLER)
+dispatcher.add_handler(RLG_HANDLER)
+dispatcher.add_handler(DECIDE_HANDLER)
+dispatcher.add_handler(TABLE_HANDLER)
+dispatcher.add_handler(RAPE_HANDLER)
+dispatcher.add_handler(ABUSE_HANDLER)
+dispatcher.add_handler(BEN_HANDLER)
+
+__mod_name__ = "Fun 2"
+__command_list__ = ["runs", "slap", "roll", "toss", "shrug", "bluetext", "rlg", "decide", "table","rape","abuse","ben"]
+__handlers__ = [ROLL_HANDLER, TOSS_HANDLER, SHRUG_HANDLER, BLUETEXT_HANDLER, RLG_HANDLER,
+                DECIDE_HANDLER, TABLE_HANDLER, RAPE_HANDLER, ABUSE_HANDLER, BEN_HANDLER]
