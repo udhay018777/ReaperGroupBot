6a0f3173c0be4d89481d5b62c31a41315e8eb688
udhay018777 <63841073+udhay018777@users.noreply.github.com>
2020-05-23 14:32:05 +0530

    Update warns.py

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -9,16 +9,16 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, run_async, DispatcherHandlerStop, MessageHandler, Filters, CallbackQueryHandler
 from telegram.utils.helpers import mention_html
 
-from haruka import dispatcher, BAN_STICKER
-from haruka.modules.disable import DisableAbleCommandHandler
-from haruka.modules.helper_funcs.chat_status import is_user_admin, bot_admin, user_admin_no_reply, user_admin, \
+from tg_bot import dispatcher, BAN_STICKER
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.chat_status import is_user_admin, bot_admin, user_admin_no_reply, user_admin, \
     can_restrict
-from haruka.modules.helper_funcs.extraction import extract_text, extract_user_and_text, extract_user
-from haruka.modules.helper_funcs.filters import CustomFilters
-from haruka.modules.helper_funcs.misc import split_message
-from haruka.modules.helper_funcs.string_handling import split_quotes
-from haruka.modules.log_channel import loggable
-from haruka.modules.sql import warns_sql as sql
+from tg_bot.modules.helper_funcs.extraction import extract_text, extract_user_and_text, extract_user
+from tg_bot.modules.helper_funcs.filters import CustomFilters
+from tg_bot.modules.helper_funcs.misc import split_message
+from tg_bot.modules.helper_funcs.string_handling import split_quotes
+from tg_bot.modules.log_channel import loggable
+from tg_bot.modules.sql import warns_sql as sql
 
 WARN_HANDLER_GROUP = 9
 CURRENT_WARNING_FILTER_STRING = "<b>Current warning filters in this chat:</b>\n"
