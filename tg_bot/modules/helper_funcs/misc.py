a9a8b36ec8bee89ae24936b4ef88fcd98ce61cec
udhay018777 <63841073+udhay018777@users.noreply.github.com>
2020-05-24 14:15:46 +0530

    Update misc.py

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -65,13 +65,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     max_num_pages = ceil(len(pairs) / 21)
     modulo_page = page_n % max_num_pages
 
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 21:21 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("◀️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("⏹", callback_data="bot_start"),
-             EqInlineKeyboardButton("▶️", callback_data="{}_next({})".format(prefix, modulo_page)))]
-
+   
     else:
         pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
 
