a3823237d4731f0945915e263c2b838969a1135e
udhay018777 <63841073+udhay018777@users.noreply.github.com>
2020-05-24 14:18:49 +0530

    Update misc.py

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -66,12 +66,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     modulo_page = page_n % max_num_pages
 
    
-    else:
-        pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
 
-
-
-    return pairs
 
 
 def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
